561f2302d59b3f0cde93a5ea6eb1baf15c19a6b6
Eduardo Valle <valle9129@gmail.com>
2025-03-03 22:03:14 -0300

    Improving class main

--- pedido/src/main/java/com/valledev/pedido/PedidoApplication.java
+++ pedido/src/main/java/com/valledev/pedido/PedidoApplication.java
@@ -20,17 +20,16 @@ public class PedidoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Order order1 = new Order(1034, 150.00, 20.0);
-		Order order2 = new Order(2282, 800.00, 10.0);
-		Order order3 = new Order(1309, 95.90, 00.0);
+		Order[] orders = {
+				new Order(1034, 150.00, 20.0),
+				new Order(2282, 800.00, 10.0),
+				new Order(1309, 95.90, 0.0)
+		};
 
-		System.out.println("Pedido código: " + order1.getCode() + "\nValor total: R$ "
-				+ String.format("%.2f", orderService.calculateTotal(order1)) + "\n");
-
-		System.out.println("Pedido código: " + order2.getCode() + "\nValor total: R$ "
-				+ String.format("%.2f", orderService.calculateTotal(order2))+ "\n");
-
-		System.out.println("Pedido código: " + order3.getCode() + "\nValor total: R$ "
-				+ String.format("%.2f", orderService.calculateTotal(order3))+ "\n");
+		for (Order order : orders) {
+			System.out.println("Pedido código: " + order.getCode());
+			System.out.println("Valor total: R$ " + String.format("%.2f", orderService.calculateTotal(order)));
+			System.out.println();
+		}
 	}
 }
